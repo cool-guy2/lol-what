b1f8ecc1834c73a2a3f0999959adf55446130ae4
cool-guy2 <60459132+cool-guy2@users.noreply.github.com>
2020-01-29 23:03:05 -0600

    Batch update

---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+
+<!DOCTYPE html><html><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700,900&amp;subset=latin-ext" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Fredoka+One|Montserrat&display=swap" rel="stylesheet">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="apple-mobile-web-app-title" content="YoHoHo-io.Space">
+    <meta name="viewport" content="minimal-ui, user-scalable=no, initial-scale=1, maximum-scale=1, width=device-width">
+    <title>🏴‍ YoHoHo io SpAce UnBLocKed</title> 
+    <meta name="description" content="YoHoHo io SpAce UnBLocKed Multiplayer survive pirate game The objective of Yohoho io is to vanquish every one of the foes. Attempt each mode the game offers and pick the one that brings generally fun.">
+    <meta property="og:title" content="YoHoHo io SpAce UnBLocKed">
+    <meta property="og:description" content="YoHoHo io SpAce UnBLocKed Multiplayer survive pirate game The objective of Yohoho io is to vanquish every one of the foes. Attempt each mode the game offers and pick the one that brings generally fun.">
+    <meta property="og:url" content="https://YoHoHo-io.Space/">
+    <meta property="og:image" content="https://yohoho-io.space/img/banner1200x630.jpg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:type" content="website">
+    <link rel="apple-touch-startup-image" href="https://yohoho-io.space/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://yohoho-io.space/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://yohoho-io.space/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://yohoho-io.space/favicon/favicon-16x16.png">
+    <link rel="manifest" href="https://yohoho-io.space/favicon/site.webmanifest">
+    <link rel="shortcut icon" href="https://yohoho-io.space/favicon/favicon.ico">
+    <meta name="theme-color" content="#ffffff">
+    <link href="https://yohoho-io.space/yohoho.css?1580359742" rel="stylesheet">
+		  <script src="https://code.jquery.com/jquery-3.4.1.min.js">
+
+if (document.location.protocol === "http:") {
+    if (document.referrer && document.location.search.length == 0) {
+        var parts = document.referrer.split('/')[2].split(".");
+        var ref = (parts[parts.length - 2] == "co" || parts[parts.length - 2] == "com") ? parts.slice(-3).join(".") : parts.slice(-2).join(".");
+        document.location.href = document.location.href.replace("http:", "https:") + "?ref=" + encodeURIComponent(ref);
+    } else document.location.href = document.location.href.replace("http:", "https:");
+}
+
+
+
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/21631214087/agariot2',[300, 250],'div-gpt-ad-1506878184940-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+  
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/21631214087/160lık', [160, 600], 'div-gpt-ad-1580146111098-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/21631214087/970lik', [970, 250], 'div-gpt-ad-1580146168486-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+
+  
